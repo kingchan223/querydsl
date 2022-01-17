4ca94fc94e86685dd091d134b04fca9e8e3aac5b
kingchan223 <kokiyo97@naver.com>
2022-01-17 10:46:24 +0900

    기본문법(정렬, 페이징, 집합, 조인)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,21 +11,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Transactional
 @SpringBootTest
 public class QuerydslBasicTest {
 
-    @Autowired
-    EntityManager em;
+    @Autowired EntityManager em;
+
     JPAQueryFactory queryFactory;
+
     @BeforeEach
     public void before(){
         queryFactory = new JPAQueryFactory(em);
@@ -35,9 +42,9 @@ public class QuerydslBasicTest {
         em.persist(teamB);
 
         Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
         Member member3 = new Member("member3", 10, teamB);
-        Member member4 = new Member("member4", 10, teamB);
+        Member member4 = new Member("member4", 20, teamB);
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
@@ -121,5 +128,200 @@ public class QuerydslBasicTest {
         //처음 한 건 조회
         Member fetchFirst = queryFactory.selectFrom(QMember.member)
                 .fetchFirst();
+    }
+    /*
+       *회원 정렬 순서
+       1. 회원 나이 내림차순(desc)
+       2. 회원 이름 올림차순(asc)
+       3. 단, 2에서 회원 이름이 없으면 마지막에 출력(null last)
+     */
+    @Test
+    void sort(){
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory.selectFrom(member)
+                .where(member.age.eq(100))//100살인 회원
+                .orderBy(member.age.desc(),//나이 내림차순
+                        member.username.desc(),
+                        member.username.asc().nullsLast())//username null이면 맨 마지막
+                .fetch();
+        Member member6 = result.get(0);
+        Member member5 = result.get(1);
+        Member memberNull = result.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+        for (Member m : result) System.out.println("m = " + m);
+    }
+
+    @Test
+    void paging1() {
+
+
+        List<Member> result = queryFactory.selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)//몇개 건너뛸건지
+                .limit(2)//몇개 뽑아낼건지
+                .fetch();
+        assertThat(result.size()).isEqualTo(2);
+        for (Member m : result) System.out.println("m = " + m);
+    }
+
+    @Test
+    void aggregation() {
+        //querydsl에서 제공하는 Tuple
+        List<Tuple> result = queryFactory.select
+                        (member.count(),
+                            member.age.sum(),
+                            member.age.avg(),
+                            member.age.max(),
+                            member.age.min())
+                .from(member).fetch();
+
+        Tuple tuple = result.get(0);
+
+        Long memberCnt = tuple.get(member.count());
+        Integer sumAge = tuple.get(member.age.sum());
+        Double avgAge = tuple.get(member.age.avg());
+        Integer maxAge = tuple.get(member.age.max());
+        Integer minAge = tuple.get(member.age.min());
+
+        assertThat(memberCnt).isEqualTo(4);
+        assertThat(sumAge).isEqualTo(40);
+        assertThat(avgAge).isEqualTo(10);
+        assertThat(maxAge).isEqualTo(10);
+        assertThat(minAge).isEqualTo(10);
+    }
+
+    /*
+    * 팀의 이름과 각 팀의 평균 연령을 구하라
+    * */
+    @Test
+    void aggregation2() {
+        List<Tuple> result = queryFactory.select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        System.out.println("teamA = " + teamA);
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+
+        System.out.println("teamB = " + teamB);
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamB.get(member.age.avg())).isEqualTo(15);
+    }
+
+    /*
+    * 팀A에 소속된 모든 회원 조회
+    * */
+    @Test
+    void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        for (Member m : result) System.out.println("m = " + m);
+    }
+
+    /*
+    * 세타조인
+    * 회원의 이름과 팀의 이름이 같은 회원 조회 =
+    * */
+    @Test
+    void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamA"));
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)//from절에 나열
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        for (Member m : result) System.out.println("m = " + m);
+    }
+
+    /*
+    * 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조회, 회원은 모두 조회
+    * JPQL : select m, t from Member m left join m.team t on t.name = 'teamA'
+    * */
+    @Test
+    void join_on_filtering() {
+        List<Tuple> result = queryFactory.select(member, team)
+                .from(member)
+                .join(member.team, team).on(team.name.eq("teamA"))//그냥 join으로
+                .fetch();
+
+        for (Tuple tuple : result)
+            System.out.println("tuple = " + tuple);
+
+    }
+
+    @Test
+    void join_where() {
+        List<Tuple> result = queryFactory.select(member, team)
+                .from(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result)
+            System.out.println("tuple = " + tuple);
+
+    }
+
+    /*
+     * 연관관계가 없는 엔티티 외부 조인
+     * 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+     * */
+    @Test
+    void theta_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple t : result) System.out.println("t = " + t);
+    }
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    void no_fetchJoin() {
+        em.flush();
+        em.clear();
+        Member findMember = queryFactory.selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        //페치 조인을 적용하지 않았으므로 team은 lazy 초기화로 아직 load되지 않은 상태이다.
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페티조인 미적용").isFalse();
+    }
+
+    @Test
+    void yes_fetchJoin() {
+        em.flush();
+        em.clear();
+        Member findMember = queryFactory.selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        //페치 조인을 적용하였으므로 Team도 같이 가져와서 team이 load됨
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페티조인 미적용").isTrue();
     }
 }
